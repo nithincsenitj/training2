cdaccb538b0629bb05aab7ab19a0d30c1f647f3d
nithinchandrank <nithincsenitj@gmail.com>
2018-10-17 17:25:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+//app.use(express.static(__dirname + '/view'));
+
+app.get('/',function(req,res) {
+  res.send('Hello world');
+})
+
+app.get('/page2',function(req,res) {
+  res.send('we are at page 2');
+})
+
+app.get('/page3',function(req,res) {
+  res.sendFile(__dirname + "/" + "view" + "/" + "page.html");
+})
+
+var server = app.listen(8081,function(){
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Listening at localhost 8081..:)")
+})
